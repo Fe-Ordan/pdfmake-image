33144794cd22260208327eb8658540f65a2ee6eb
ngdevelop-tech <aprajapati06@gmail.com>
2019-11-17 08:19:14 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,9 @@ export class AppComponent {
     let docDefinition = {
       content: [
         {
-          image:
-            (await this.getBase64ImageFromURL(
-              "https://images.pexels.com/photos/209640/pexels-photo-209640.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300"
-            ))
+          image: await this.getBase64ImageFromURL(
+            "https://images.pexels.com/photos/209640/pexels-photo-209640.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300"
+          )
         }
       ]
     };
@@ -26,42 +25,29 @@ export class AppComponent {
     pdfMake.createPdf(docDefinition).download();
   }
 
-  getBase64ImageFromURL(url: string): Promise<string> {
+  getBase64ImageFromURL(url) {
     return new Promise((resolve, reject) => {
-      // create an image object
-      let img = new Image();
-      img.crossOrigin = "Anonymous";
+      var img = new Image();
+      img.setAttribute("crossOrigin", "anonymous");
+
+      img.onload = () => {
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+
+        var dataURL = canvas.toDataURL("image/png");
+
+        resolve(dataURL);
+      };
+
+      img.onerror = error => {
+        reject(error);
+      };
+
       img.src = url;
-      if (!img.complete) {
-        // This will call another method that will create image from url
-        img.onload = () => {
-          resolve(this.getBase64Image(img));
-        };
-        img.onerror = err => {
-          reject(err);
-        };
-      } else {
-        resolve(this.getBase64Image(img));
-      }
     });
-  }
-
-  getBase64Image(img: HTMLImageElement) {
-    // We create a HTML canvas object that will create a 2d image
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    var ctx = canvas.getContext("2d");
-    // This will draw image
-    ctx.drawImage(img, 0, 0);
-    // Convert the drawn image to Data URL
-    var dataURL = canvas.toDataURL("image/png");
-    return dataURL;
-  }
-
-  toDataUrl(url) {
-    return this.http
-      .get(url, { responseType: "blob" })
-      .pipe(tap(data => console.log(data)));
   }
 }
